87afaec4b08ecf5478262fe2c7d75aab7e686ecc
fardinkhan <fardinkhan@uvic.ca>
2018-12-30 19:07:59 -0800

    extremely easy

---
+++ hack_python2.py
@@ -0,0 +1,20 @@
+"""
+Task 
+Read two integers from STDIN and print three lines where:
+
+The first line contains the sum of the two numbers.
+The second line contains the difference of the two numbers (first - second).
+The third line contains the product of the two numbers.
+
+Constraints: 
+1 <= a <= 10**10
+Same for b
+"""
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+
+    print(a+b)
+    print(a-b)
+    print(a * b)
